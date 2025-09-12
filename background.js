78ebb4839ba587ae5c0425051c87fae8293dce48
Xamez <35816109+Xamez@users.noreply.github.com>
2025-09-12 20:42:23 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.tabs.onCreated.addListener(async (tab) => {    
+    if (tab.url === '') {    
+        const window = await chrome.windows.get(tab.windowId);
+        if (window.state === 'fullscreen') {
+            await chrome.windows.update(window.id, { state: 'maximized' });
+            await chrome.tabs.create({ windowId: window.id, active: true });
+            chrome.tabs.remove(tab.id);
+        }
+    }
+});
