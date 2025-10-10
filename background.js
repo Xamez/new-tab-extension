3d41a586d8157df5fa6e1a48747c290b6cd68e00
Xamez <35816109+Xamez@users.noreply.github.com>
2025-10-10 20:13:21 +0200

    fix: improve new tab detection logic for fullscreen windows

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
-chrome.tabs.onCreated.addListener(async (tab) => {    
-    if (tab.url === '' && !tab.openerTabId && !tab.pendingUrl) {    
+chrome.tabs.onCreated.addListener(async (tab) => {
+    const isNewTabPage = tab.pendingUrl.startsWith('edge://newtab');
+    if (tab.url === '' && isNewTabPage) {
         const window = await chrome.windows.get(tab.windowId);
         if (window.state === 'fullscreen') {
             await chrome.windows.update(window.id, { state: 'maximized' });
